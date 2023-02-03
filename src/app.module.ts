f7031bb474dcdc7faa67ba432989518729f4131c
pavel asadchy <asadchy.pavel@gmail.com>
2023-02-03 23:56:11 +0100

    feat: init resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumsModule } from './resources/albums/albums.module';
+import { UsersModule } from './resources/users/users.module';
+import { ArtistsModule } from './resources/artists/artists.module';
+import { TracksModule } from './resources/tracks/tracks.module';
+import { FavoritesModule } from './resources/favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AlbumsModule,
+    UsersModule,
+    ArtistsModule,
+    TracksModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
